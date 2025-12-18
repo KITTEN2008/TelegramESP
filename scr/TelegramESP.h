0e75f24ddaa593b3747136d5984facff728fc396
KITTEN2008 <elt.sans@bk.ru>
2025-12-18 12:08:45 +0300

    Создание заголовка TelegramESP.h

---
+++ scr/TelegramESP.h
@@ -0,0 +1,26 @@
+#ifndef UniversalTelegramBot_h
+#define UniversalTelegramBot_h
+
+#define ARDUINOJSON_DECODE_UNICODE 1
+#define ARDUINOJSON_USE_LONG_LONG 1
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <Client.h>
+#include <TelegramCertificate.h>
+
+#define TELEGRAM_HOST "api.telegram.org"
+#define TELEGRAM_SSL_PORT 443
+#define HANDLE_MESSAGES 1
+
+typedef bool (*MoreDataAvailable)();
+typedef byte (*GetNextByte)();
+typedef byte* (*GetNextBuffer)();
+typedef int (GetNextBufferLen)();
+
+struct telegramMessage {
+  String text;
+  String chat_id;
+  String chat_title;
+  String from_id;
+
+#endif
